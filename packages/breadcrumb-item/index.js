d1d39bbf3e96d35fd1f68f99498768b4f42a2411
zengyuanqiu <zengyuanqiu@gmail.com>
2018-11-27 10:27:46 +0800

    add breadcrumb-item|index.js

---
+++ packages/breadcrumb-item/index.js
@@ -0,0 +1,7 @@
+import ElBreadcrumbItem from '../breadcrumb/src/breadcrumb-item.vue';
+
+ElBreadcrumbItem.install = function(Vue) {
+	Vue.component(ElBreadcrumbItem.name, ElBreadcrumbItem)
+}
+
+export default ElBreadcrumbItem;
