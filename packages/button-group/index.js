0cc48e9b05a8efd9181a7bc9cdc91d2bdd84988c
zengyuanqiu <zengyuanqiu@gmail.com>
2018-11-27 11:01:52 +0800

    add button-group|index.js

---
+++ packages/button-group/index.js
@@ -0,0 +1,7 @@
+import ElButtonGroup from '../button/src/button-group';
+
+ElButtonGroup.install = function(Vue) {
+	Vue.component(ElButtonGroup.name, ElButtonGroup)
+}
+
+export default ElButtonGroup;
